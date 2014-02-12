6a0b27e8207699b74751c0e03fd91b906f260afc
Greg Nowicki <nowicki.greg@gmail.com>
2014-02-12 01:43:43 -0500

    Created the app and added twitter bootstrap. Also created a bootstrap theme for the links model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Networkme::Application.config.secret_key_base = 'add08f19db803b681bb8d4053ceac18d3461024042ee6c410636fd5d9e9baa019661d6e9548f80e2561dcbc2db0b1640b9f268fb4c2a998ff1820805ca505d79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Networkme::Application.config.session_store :cookie_store, key: '_networkme_session'
